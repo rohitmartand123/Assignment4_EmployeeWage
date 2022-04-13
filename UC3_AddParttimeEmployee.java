d0209cd3e5bef4a4d1378201ac6fbb7c61ce67d1
rohitmartand123 <rohitmartand274@gmail.com>
2022-04-13 13:04:27 +0530

    Refactor : Employeewage

---
+++ UC3_AddParttimeEmployee.java
@@ -0,0 +1,10 @@
+package com.BridgeLabzAssignment4Employeewage;
+
+public class UC3_AddParttimeEmployee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
