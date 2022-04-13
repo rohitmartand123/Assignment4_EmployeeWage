44b404ab1be88a1372459e1cf1373eac10eda0cb
rohitmartand123 <rohitmartand274@gmail.com>
2022-04-13 13:00:06 +0530

    Refactor : Employeewage

---
+++ UC2_CalculatingEmployeeWage.java
@@ -0,0 +1,10 @@
+package com.BridgeLabzAssignment4Employeewage;
+
+public class UC2_CalculatingEmployeeWage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
