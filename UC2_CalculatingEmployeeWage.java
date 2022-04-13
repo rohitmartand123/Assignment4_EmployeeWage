893640e3c94f39c81ab669ec31ae198f693c9499
rohitmartand123 <rohitmartand274@gmail.com>
2022-04-13 13:54:46 +0530

    Refactor : Employeewage

--- UC2_CalculatingEmployeeWage.java
+++ UC2_CalculatingEmployeeWage.java
@@ -1,10 +1,22 @@
 package com.BridgeLabzAssignment4Employeewage;
-
+/*
+ * @author Rohit
+ * Calculate Daily Employee Wage
+ */
 public class UC2_CalculatingEmployeeWage {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int IS_FULL_TIME = 1;
+		int EMP_RATE_PER_HOUR = 20;
+		int  empHrs= 0;
+		int empWage = 0;
+		double empCheck = Math.floor(Math.random()* 10) %2;
+		if (empCheck == IS_FULL_TIME)
+		empHrs = 8;
+		else
+			empHrs= 0;
+		empWage = empHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Emp Wage: " + empWage);
 	}
 
 }
