90cbd367112035c491a951f4a33baf829ca78cdf
rohitmartand123 <rohitmartand274@gmail.com>
2022-04-13 12:50:20 +0530

    Added : Employeewage

---
+++ UC1_CheckEmployeePresence.java
@@ -0,0 +1,17 @@
+package com.BridgeLabzAssignment4Employeewage;
+/*
+ * @author Rohit
+ * Check Employee is Present or Absent
+ */
+public class UC1_CheckEmployeePresence {
+
+	public static void main(String[] args) {
+		int IS_FULL_TIME = 1;
+		double empCheck = Math.floor(Math.random()* 10) %2;
+		if (empCheck == IS_FULL_TIME)
+			System.out.println("Employee is present");
+		else
+			System.out.println("Employee is Absent");
+	}
+
+}
