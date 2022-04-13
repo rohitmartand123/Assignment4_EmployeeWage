0176ee326509780de55d535c9eac406fb4c164fa
rohitmartand123 <rohitmartand274@gmail.com>
2022-04-13 13:42:24 +0530

    Refactor : Employeewage

---
+++ UC4_CalculatingEmployeeWageUsingSwitch.java
@@ -0,0 +1,10 @@
+package com.BridgeLabzAssignment4Employeewage;
+
+public class UC4_CalculatingEmployeeWageUsingSwitch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
